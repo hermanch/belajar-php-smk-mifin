5a7ed2966746fe6e573032f922f9b4fc6942f06e
hermanch <herman.btu@gmail.com>
2021-12-24 15:55:13 +0700

    bab-1

---
+++ bab_01_pengertian_php/02.daftar_mahasiswa.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+  <meta charset="UTF-8">
+  <title>Daftar Mahasiswa</title>
+</head>
+<body>
+<h2>Daftar Absensi Mahasiswa</h2>
+  <ol>
+    <?php
+    for ($i= 1; $i <= 10; $i++) {
+      echo "<li>Nama Mahasiswa ke-$i</li>";
+    }
+    ?>
+  </ol>
+</body>
+</html>
